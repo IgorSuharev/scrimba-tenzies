b49941f9d876a85aa44954883be62af7feec5e82
Igor Suharev <new dot igorsuharev at gmail.com>
2023-10-16 12:54:07 +0300

    Add some rules to ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,12 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'semi': ['error'],
-    
+    'no-constructor-return': ['error'],
+    'array-callback-return': ['error'],
+    'no-await-in-loop': ['error'],
+    'no-constant-binary-expression': ['error'],
+    'no-duplicate-imports': ['error'],
+
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
