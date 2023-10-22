f82ef3a66278ba979620eae5cfbfa53a3377c827
Igor Suharev <new dot igorsuharev at gmail.com>
2023-10-22 21:49:44 +0300

    Migrate Header to Tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <h1 className="m-0 font-karla text-2xl tracking-tight text-sky-950">
+        Tenzies
+      </h1>
+      <p className="m-0 w-60 font-inter text-xs tracking-tight text-slate-600">
+        Roll until all dice are the same. Click each die to freeze it at its current value between
+        rolls.
+      </p>
+    </header>
+  );
+}
