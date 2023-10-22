cdc0310f8feb0652ecaa3f2a5455d63a6a4ad83c
Igor Suharev <new dot igorsuharev at gmail.com>
2023-10-22 21:58:42 +0300

    Migrate Die to Tailwind

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,13 +1,12 @@
-import styles from './Die.module.css';
 import classNames from 'classnames';
 
 export default function Die(props) {
   return (
     <button
       className={classNames(
-        styles.die, {
-          [styles.die_frozen]: props.frozen,
-          [styles.die_success]: props.success
+        'w-9 h-9 rounded shadow-md hover:shadow-lg active:shadow-inner bg-white font-karla text-xl text-slate-950', {
+          ['bg-sky-300']: props.frozen,
+          ['bg-yellow-400']: props.success
         }
       )}
       onClick={props.onClick}
